e6d1c1cfbe4b1bc475930c06445067b305dfb7ee
Harrison Nguyen <nguy2173@umn.edu>
2019-12-14 16:21:54 -0600

    Map through reducers, set to local state in review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 class Review extends Component {
+
+    state = {
+        feelings: this.props.reduxState.feelingReducer.map( (value, i) => {
+            return(<p key={i} value={value}>Feelings: {value.feelings}</p>)
+        }),
+        understand: this.props.reduxState.understandReducer.map( (value, i) => {
+            return(<p key={i} value={value}>Understanding: {value.understand}</p>)
+        }),
+        support: this.props.reduxState.supportReducer.map( (value, i) => {
+            return(<p key={i} value={value}>Support: {value.support}</p>)
+        }),
+        comment: this.props.reduxState.commentReducer.map( (value, i) => {
+            return(<p key={i} value={value}>Comments: {value.comment}</p>)
+        })
+
+    }
+
+    handleSubmit = () => {
+
+    }
+
     render() {
         return (
             <section>
                 <header>
                         <h1>Review Your Feedback</h1>
                     </header>
-                <form>
-                    {this.props.reduxState.feelingReducer.map( (value, i) => {
-                        return(<p key={i} value={value}>Feelings: {value.feelings}</p>)
-                    })}
-
-                    {this.props.reduxState.understandReducer.map( (value, i) => {
-                        return(<p key={i} value={value}>Understanding: {value.understand}</p>)
-                    })}
-
-                    {this.props.reduxState.supportReducer.map( (value, i) => {
-                        return(<p key={i} value={value}>Support: {value.support}</p>)
-                    })}
-                    
-                    {this.props.reduxState.commentReducer.map( (value, i) => {
-                        return(<p key={i} value={value}>Comments: {value.comment}</p>)
-                    })}
-                    <button>Submit</button>
-                </form>
+                <div>
+                    {this.state.feelings}
+                    {this.state.understand}
+                    {this.state.support}
+                    {this.state.comment}
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                </div>
             </section>
         );
     }
