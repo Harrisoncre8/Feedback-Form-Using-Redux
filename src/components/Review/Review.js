6a98532aebdaf64289206bb6a0be54e256537d5c
Harrison Nguyen <nguy2173@umn.edu>
2019-12-14 16:07:54 -0600

    Render from redux in Review Component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 class Review extends Component {
     render() {
         return (
-            <>
-            </>
+            <section>
+                <header>
+                        <h1>Review Your Feedback</h1>
+                    </header>
+                <form>
+                    {this.props.reduxState.feelingReducer.map( (value, i) => {
+                        return(<p key={i} value={value}>Feelings: {value.feelings}</p>)
+                    })}
+
+                    {this.props.reduxState.understandReducer.map( (value, i) => {
+                        return(<p key={i} value={value}>Understanding: {value.understand}</p>)
+                    })}
+
+                    {this.props.reduxState.supportReducer.map( (value, i) => {
+                        return(<p key={i} value={value}>Support: {value.support}</p>)
+                    })}
+                    
+                    {this.props.reduxState.commentReducer.map( (value, i) => {
+                        return(<p key={i} value={value}>Comments: {value.comment}</p>)
+                    })}
+                    <button>Submit</button>
+                </form>
+            </section>
         );
     }
 }
 
-export default Review;
+const putPropsOnReduxState = (reduxState) => ({
+    reduxState
+})
+
+export default withRouter(connect(putPropsOnReduxState)(Review));
